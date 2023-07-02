9700bd9ef1a054ae6fc817abb6851e588e7adaa3
Aljosa Janjic <janjic_aljosa@hotmail.com>
2023-07-02 12:18:05 +0200

    Multiple Interceptors

---
+++ src/app/logging-interceptor.service.ts
@@ -0,0 +1,16 @@
+import { HttpEvent, HttpEventType, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { tap } from "rxjs/operators";
+
+export class LoggingInterceptorService implements HttpInterceptor {
+    intercept(req: HttpRequest<any>, next: HttpHandler){
+        console.log('Outgoing resutest');
+        console.log(req.url)
+        return next.handle(req).pipe(tap(event => {
+            if (event.type == HttpEventType.Response){
+                console.log('Response arrived, body data: ');
+                console.log(event.body)
+            }
+        }));
+    }
+}
